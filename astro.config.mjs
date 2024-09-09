56b544917b881d38d027ab575bd564c5c02c2466
Kevin F <Freymaurer@gmx.de>
2024-09-09 11:43:52 +0200

    add publish workflow file :sparkles:

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://freymaurer.github.io',
+  base: 'astro-poc'
 });
