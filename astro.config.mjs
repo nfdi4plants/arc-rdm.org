8e12b7dfcc5874daf0b80ac6bc611fd823233e0c
Kevin F <Freymaurer@gmx.de>
2024-09-10 12:10:51 +0200

    add autolink with rehype

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,34 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://freymaurer.github.io',
   base: 'astro-poc',
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'prepend',
+          content: {
+            type: 'text',
+            value: '#',
+          },
+          headingProperties: {
+            className: ['anchor'],
+          },
+          properties: {
+            className: ['anchor-link'],
+          },
+        },
+      ]
+    ]
+  },
 });
