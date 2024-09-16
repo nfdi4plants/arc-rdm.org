58378acefbf85e44febf8db343583ed82708450d
HLWeil <hl.weil@t-online.de>
2024-09-16 10:53:36 +0200

    create and use path aliases for existing documentation entries

--- src/statics.ts
+++ src/statics.ts
@@ -5,6 +5,17 @@ export enum URLS {
   Internal_Home = BASE_PATH,
   Internal_Tools = BASE_PATH + "/details/tools-and-services",
 
+  Internal_Com_DataManagement = BASE_PATH + "/details/data-management-principle",
+  Internal_Com_Tools = BASE_PATH + "/details/tools-and-services",
+  Internal_Com_Documentation = BASE_PATH + "/details/documentation-principle",
+  Internal_Com_Organization = BASE_PATH + "/details/organization-principle",
+  Internal_Com_Fairness = BASE_PATH + "/details/fairness-and-rdm",
+
+  Internal_Dev_DataModel = BASE_PATH + "/details/arc-data-model",
+  Internal_Dev_Representation = BASE_PATH + "/details/arc-representation",
+  Internal_Dev_FDO = BASE_PATH + "/details/arc-fdo",
+
+
   GITHUB_REPO = "https://github.com/nfdi4plants/arc-website"
 }
 
