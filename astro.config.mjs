45328b0a572f5fcd547541c2bd6c956b40ab4257
Kevin F <Freymaurer@gmx.de>
2024-09-17 10:45:47 +0200

    add tool sub-md support

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,13 @@ import {remarkReplaceTooltips} from './src/plugins/remark-dictionary-tooltips.ts
 import {remarkAsides} from './src/plugins/remark-asides.ts';
 import remarkDirective from 'remark-directive';
 import mdx from '@astrojs/mdx';
+
+import icon from 'astro-icon';
 import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), pagefind()],
+  integrations: [tailwind(), mdx(), icon(), pagefind()],
   site: 'https://nfdi4plants.github.io',
   base: 'arc-website',
   trailingSlash: "always", // will kill pagefind integration without
