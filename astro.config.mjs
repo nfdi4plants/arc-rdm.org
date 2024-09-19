372f91b1952bbf00da5c712ec0a4852515e59d5e
Kevin F <Freymaurer@gmx.de>
2024-09-19 14:34:16 +0200

    delete test file

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ import pagefind from "astro-pagefind";
 export default defineConfig({
   integrations: [tailwind(), mdx(), icon(), pagefind()],
   site: 'https://nfdi4plants.github.io',
-  base: 'arc-website/', // will kill pagefind integration without trailing slash
+  base: '/arc-website/', // will kill pagefind integration without trailing slash
   markdown: {
     remarkPlugins: [remarkDirective, remarkReplaceLinks(), remarkReplaceTooltips(), remarkAsides()],
     rehypePlugins: [
