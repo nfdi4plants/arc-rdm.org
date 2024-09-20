1dde7c15b112fd34c890f85a1cd8f9845e57a4a0
Kevin F <Freymaurer@gmx.de>
2024-09-20 13:55:01 +0200

    fixed site

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -4,10 +4,15 @@ const getRobotsTxt = (sitemapURL: URL) => `
 User-agent: *
 Allow: /
 
-Sitemap: ${sitemapURL.href + sitemapURL.pathname}
+Sitemap: ${sitemapURL.href}
 `;
 
 export const GET: APIRoute = ({ site }) => {
+  console.log(site);
+  if (!site) {
+    return new Response('No site object found', { status: 500 });
+  }
+  site.pathname = import.meta.env.BASE_URL;
   const sitemapURL = new URL('sitemap-index.xml', site);
   return new Response(getRobotsTxt(sitemapURL));
 };
